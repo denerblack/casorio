ab13b127c4e1c36f1bdb486f8d918326de53eb30
denerBlack <dener.wilian@gmail.com>
2013-12-29 22:38:43 -0300

    adicionado devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,12 +18,13 @@ Casorio::Application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
+  # This option may cause significant delays in view renderi#ng with a large
   # number of complex assets.
   config.assets.debug = true
   config.i18n.available_locales = ['pt-BR', :pt]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Casorio::Application.routes.draw do
+  devise_for :admins
   devise_for :users
   #devise_for :users
   resources :guests
