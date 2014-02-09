4b9c457d9849cdda1a8801e99b52c562e0dd9984
Dener W P do Carmo <dener.wilian@gmail.com>
2014-02-08 23:45:24 -0300

    Totalizando

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -70,6 +70,6 @@ class GuestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def guest_params
-      params.require(:guest).permit(:name, :companions_amount, :confirmed)
+      params.require(:guest).permit(:name, :companions_amount, :children_amount, :confirmed)
     end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,2 +1,4 @@
 class Guest < ActiveRecord::Base
+	scope :total_companions, -> {where.not(companions_amount: 0).map(&:companions_amount).sum}
+	scope :total_children, -> {where.not(children_amount: 0).map(&:children_amount).sum}
 end
