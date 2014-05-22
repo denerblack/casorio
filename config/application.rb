10753d7eb93e8e8212c9319045f440c812e46807
Dener W P do Carmo <dener.wilian@gmail.com>
2014-05-22 17:49:55 -0400

    Ajustando locales

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Casorio
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-		config.i18n.available_locales = ["pt-BR"]
+		I18n.config.enforce_available_locales = false
     config.i18n.default_locale = :'pt-BR'
 		config.autoload_paths += %W(#{config.root}/lib)
 		config.paths['app/views'] << "app/views/devise"
