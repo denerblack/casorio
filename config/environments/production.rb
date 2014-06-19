083e98433797eb42a3c462155bebc2e971a66c5e
Dener W P do Carmo <dener.wilian@gmail.com>
2014-06-19 15:39:37 -0400

    Ajustado xeditable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,6 @@ Casorio::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-	config.logger = Logger.new(STDOUT)
   # Set to :debug to see everything in the log.
   config.log_level = :debug
 
