15251f634442af8d6fd8b69aaa4fb9d839eef5cb
Dener W P do Carmo <dener.wilian@gmail.com>
2014-06-20 08:49:45 -0400

    Ajustado colunas do prawn

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,25 +3,17 @@ class Event < ActiveRecord::Base
 
 	def generate_gift_lists_pdf
 		pdf = Prawn::Document.new
-		pdf.define_grid(columns: 2, rows: self.gift_lists.size, gutter: 20)
+		pdf.define_grid(columns: 3, rows: self.gift_lists.size, gutter: 20)
 		#pdf.grids.show_all
 		#pdf.start_new_page
 
 		pdf.move_down(30)
 
-		items = self.gift_lists.map{|gift| [gift.name]}
+		items = self.gift_lists.each_with_index.map{|gift, i| [i+1, gift.name, gift.guest_name]}
 
 		pdf.table items, :row_colors => ["FFFFFF","DDDDDD"]
 
 		pdf.move_down(10)
-
-
 		pdf
-
-		#pdf.grid([2,2], [4,4]).bounding_box do
-		#	pdf.move_down 100
-#
-#		end
-
 	end
 end
