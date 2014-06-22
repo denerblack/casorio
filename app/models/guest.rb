93131c910266aa6758e1edf72acbe2813dbf9d4b
Dener W P do Carmo <dener.wilian@gmail.com>
2014-06-22 10:19:32 -0400

    Busca em convidados

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,7 @@
 class Guest < ActiveRecord::Base
 	scope :total_companions, -> {where.not(companions_amount: 0).map(&:companions_amount).sum}
 	scope :total_children, -> {where.not(children_amount: 0).map(&:children_amount).sum}
-	scope :by_name, -> name { where(' lower(name) like ? ', "%#{name}%") }
+	scope :by_name, -> name { where(' lower(name) like ? ', "%#{name.downcase}%") }
 
 	validates_presence_of :name
 
