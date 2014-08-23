163d65360651767aa4667f04581598bb38b675dd
Dener W P do Carmo <dener.wilian@gmail.com>
2014-08-23 15:14:29 -0400

    Adicionado telefone ao parametro

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -78,6 +78,6 @@ class GuestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def guest_params
-      params.require(:guest).permit(:name, :companions_amount, :children_amount, :confirmed, :user_id)
+      params.require(:guest).permit(:name, :companions_amount, :children_amount, :confirmed, :user_id, :phone_number)
     end
 end
